e523f14337f7edb536f3a187da79a1db7fec160a
Dhruv Joshi <jdhruv036@gmail.com>
2024-11-17 09:51:40 +0530

    Services Section !!

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+
 const menuButton = document.getElementById("menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
 
 menuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+window.addEventListener('load', () => {
+  document.getElementById('preloader').style.display = 'none';
+  document.getElementById('main').classList.remove('hidden');
+  gsap.from('main', {
+    opacity: 0,
+    duration: 1.5
+  })
+})
 
 let headerText = document.getElementById("header-text");
 let textArray = [
@@ -36,3 +48,14 @@ scrollToTopBtn.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Swiper js for pagination 
+
+const swiperElement = document.getElementsByTagName('swiper-container')
+
+var swiper = new Swiper('.swiper', {
+  pagination : {
+    el : '.swiper-pagination',
+    dynamicBullets: true
+  }
+})
